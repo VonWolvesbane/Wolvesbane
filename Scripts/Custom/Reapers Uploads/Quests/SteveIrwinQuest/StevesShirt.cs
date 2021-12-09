f3bfae495495dd84a1f8b969b7bcf70973fdf11e
VonWolvesbane <57732336+VonWolvesbane@users.noreply.github.com>
2021-12-09 00:14:46 -0500

    Update StevesShirt.cs
    Fixed issue with skills, and no longer need to re-equip on login

--- Scripts/Custom/Reapers Uploads/Quests/SteveIrwinQuest/StevesShirt.cs
+++ Scripts/Custom/Reapers Uploads/Quests/SteveIrwinQuest/StevesShirt.cs
@@ -4,78 +4,22 @@ using Server.Misc;
 namespace Server.Items
 {
 	[Flipable (0x1F04, 0x1F03)] 
-	public class StevesShirt : BaseShirt 
-	{ 
-		private SkillMod m_SkillMod0; 
-		private SkillMod m_SkillMod1; 
-		private SkillMod m_SkillMod2;
-        private SkillMod m_SkillMod3; 
-		//private StatMod m_StatMod0; 
-		
+	public class StevesShirt : BaseShirt
+	{ 	
 		[Constructable] 
 		public StevesShirt() : base( 0x1EFD ) 
 		{ 
 			Name = "Steve Irwins Shirt Of Nature";
             Weight = 1.0; 
             Layer = Layer.Shirt;
-			Hue = 67; 
-			DefineMods();
+			Hue = 67;
+			this.SkillBonuses.SetValues(0, SkillName.AnimalTaming, 15.0);
+			this.SkillBonuses.SetValues(1, SkillName.AnimalLore, 15.0);
+			this.SkillBonuses.SetValues(2, SkillName.Veterinary, 15.0);
+			this.SkillBonuses.SetValues(3, SkillName.Healing, 15.0);
 		} 
 
-		private void DefineMods()
-		{
-			m_SkillMod0 = new DefaultSkillMod( SkillName.AnimalTaming, true, 15 ); 
-			m_SkillMod1 = new DefaultSkillMod( SkillName.AnimalLore, true, 15 ); 
-			m_SkillMod2 = new DefaultSkillMod( SkillName.Healing, true, 15 );
-            m_SkillMod3 = new DefaultSkillMod( SkillName.Veterinary, true, 15 ); 
-			//m_StatMod0 = new StatMod( StatType.Int, "StevesShirt", 15, TimeSpan.Zero ); 
-		}
-
-		private void SetMods( Mobile wearer )
-		{			
-			wearer.AddSkillMod( m_SkillMod0 ); 
-			wearer.AddSkillMod( m_SkillMod1 ); 
-			wearer.AddSkillMod( m_SkillMod2 );
-            wearer.AddSkillMod( m_SkillMod3 ); 
-			//wearer.AddStatMod( m_StatMod0 ); 
-		}
-
-		public override bool OnEquip( Mobile from ) 
-		{ 
-			SetMods( from );
-			return true;  
-		} 
-
-		public override bool Dye( Mobile from, DyeTub sender )
-		{
-			from.SendLocalizedMessage( 1042083 ); // You cannot dye that.
-			return false;
-		}
-
-		public override void OnRemoved( object parent ) 
-		{ 
-			if ( parent is Mobile ) 
-			{ 
-				//Mobile m = (Mobile)parent;
-				//m.RemoveStatMod( "StevesShirt" ); 
-
-				//if ( m.Hits > m.HitsMax )
-					//m.Hits = m.HitsMax; 
-
-				if ( m_SkillMod0 != null ) 
-					m_SkillMod0.Remove(); 
-
-				if ( m_SkillMod1 != null ) 
-					m_SkillMod1.Remove(); 
-
-				if ( m_SkillMod2 != null ) 
-					m_SkillMod2.Remove(); 
-			
-			    if ( m_SkillMod3 != null ) 
-					m_SkillMod3.Remove();
-			
-			} 
-		} 
+		
 
 		public override void OnSingleClick( Mobile from ) 
 		{ 
@@ -84,10 +28,7 @@ namespace Server.Items
 
 		public StevesShirt( Serial serial ) : base( serial ) 
 		{ 
-			DefineMods();
 			
-			if ( Parent != null && this.Parent is Mobile ) 
-				SetMods( (Mobile)Parent );
 		} 
 
 		public override void Serialize( GenericWriter writer ) 
