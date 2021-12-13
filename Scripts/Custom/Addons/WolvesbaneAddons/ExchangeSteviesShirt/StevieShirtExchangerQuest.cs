3cb03c3a5a4d87299059dce22195c58df12bbad7
credzba <credzba@gmail.com>
2021-12-12 19:44:06 -0600

    Add Stevie shirt exchange for gargs

---
+++ Scripts/Custom/Addons/WolvesbaneAddons/ExchangeSteviesShirt/StevieShirtExchangerQuest.cs
@@ -0,0 +1,144 @@
+﻿// Make an NPC that will exchange Stevies Shirt for a Garg Tamers Robe
+// We'll make sure the player is a garg, and they must be at least Noble to the garg queen
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Server.Items;
+using Server.Targeting;
+using Server.ContextMenus;
+using Server.Gumps;
+using Server.Misc;
+using Server.Network;
+using Server.Spells;
+namespace Server.Mobiles
+{
+	[CorpseName("Gargoyle")]
+	public class StevieShirtExchanger : Mobile
+	{
+		public virtual bool IsInvulnerable { get { return true; } }
+		[Constructable]
+		public StevieShirtExchanger()
+		{
+
+			///////////STR/DEX/INT
+			InitStats(31, 41, 51);
+
+			///////////name
+			Name = "Steglf";
+
+			///////////title
+			Title = "[Noble]";
+
+			///////////Garg.
+			Body = 0x0004;
+
+			///////////skincolor
+			Hue = Utility.RandomSkinHue();
+
+			///////////Random hair and haircolor
+			Utility.AssignRandomHair(this);
+
+			///////////clothing and hues
+			AddItem(new Server.Items.FancyShirt(Utility.RandomBlueHue()));
+			AddItem(new Server.Items.LongPants(Utility.RandomBlueHue()));
+			AddItem(new Server.Items.Sandals(Utility.RandomBlueHue()));
+
+			///////////immortal and frozen to-the-spot features below:
+			Blessed = true;
+			CantWalk = false;
+
+			///////////Adding a backpack
+			Container pack = new Backpack();
+			pack.DropItem(new Gold(250, 300));
+			pack.Movable = false;
+			AddItem(pack);
+		}
+
+		public StevieShirtExchanger(Serial serial) : base(serial) { }
+		public override void GetContextMenuEntries(Mobile from, List<ContextMenuEntry> list)
+		{
+			base.GetContextMenuEntries(from, list); 
+			list.Add(new StevieShirtExchangerEntry(from, this));
+		}
+		public override void Serialize(GenericWriter writer) 
+		{ 
+			base.Serialize(writer); 
+			writer.Write((int)0); 
+		}
+		public override void Deserialize(GenericReader reader) 
+		{ 
+			base.Deserialize(reader); 
+			int version = reader.ReadInt(); 
+		}
+		public class StevieShirtExchangerEntry : ContextMenuEntry
+		{
+			private Mobile m_Mobile; private Mobile m_Giver;
+			public StevieShirtExchangerEntry(Mobile from, Mobile giver) 
+				: base(6146, 3) 
+			{ 
+				m_Mobile = from; 
+				m_Giver = giver; 
+			}
+			public override void OnClick()
+			{
+				if (!(m_Mobile is PlayerMobile)) 
+					return;
+				PlayerMobile mobile = (PlayerMobile)m_Mobile;
+				{
+					if (mobile.Race != Race.Gargoyle)
+					{
+						if (!mobile.HasGump(typeof(StevieShirtExchangerQuestGump3)))
+						{
+							mobile.SendGump(new StevieShirtExchangerQuestGump3(mobile));
+						}
+						return;
+					}
+
+					if (Server.Engines.Points.PointsSystem.QueensLoyalty.IsNoble(mobile))
+					{
+						if (!mobile.HasGump(typeof(StevieShirtExchangerQuestGump1)))
+						{
+							mobile.SendGump(new StevieShirtExchangerQuestGump1(mobile));
+						}
+					}
+					else 
+					{
+						if (!mobile.HasGump(typeof(StevieShirtExchangerQuestGump2)))
+						{
+							mobile.SendGump(new StevieShirtExchangerQuestGump2(mobile));
+						}
+					}
+				}
+			}
+		}
+		public override bool OnDragDrop(Mobile from, Item dropped)
+		{
+			PlayerMobile mobile = from as PlayerMobile;
+			if (mobile == null)
+			{
+				return false;
+			}
+
+			if (mobile.Race == Race.Gargoyle)
+			{
+
+				//double points = Server.Engines.Points.PointsSystem.QueensLoyalty.GetPoints(from);
+				if (Server.Engines.Points.PointsSystem.QueensLoyalty.IsNoble(mobile))
+				{
+					if (dropped is StevesShirt)
+					{
+						dropped.Delete();
+
+						mobile.AddToBackpack(new RoyalGargishTamingSash());
+
+						this.PrivateOverheadMessage(MessageType.Regular, 1153, false, "Your service to the queen is appreciated", mobile.NetState);
+
+						return true;
+					}
+				}
+			}
+			this.PrivateOverheadMessage(MessageType.Regular, 1153, false, "Be gone!", mobile.NetState);
+			return false;
+		}
+	}
+}
---
+++ Scripts/Custom/Addons/WolvesbaneAddons/ExchangeSteviesShirt/StevieShirtExchangerQuestGumps.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Server;
+using Server.Commands;
+using Server.Gumps;
+using Server.Network;
+using Server.Items;
+using Server.Mobiles;
+namespace Server.Gumps
+{
+
+	public class StevieShirtExchangerQuestGump1 : Gump
+	{
+		public static void Initialize()
+		{
+			CommandSystem.Register("StevieShirtExchangerQuestGump", AccessLevel.GameMaster, new CommandEventHandler(StevieShirtExchangerQuestGump_OnCommand1));
+		}
+		private static void StevieShirtExchangerQuestGump_OnCommand1(CommandEventArgs e)
+		{
+			e.Mobile.SendGump(new StevieShirtExchangerQuestGump1(e.Mobile));
+		}
+		public StevieShirtExchangerQuestGump1(Mobile owner) : base(50, 50)
+		{
+			//----------------------------------------------------------------------------------------------------
+			AddPage(0); AddImageTiled(54, 33, 369, 400, 2624); AddAlphaRegion(54, 33, 369, 400); AddImageTiled(416, 39, 44, 389, 203);
+			//--------------------------------------Window size bar--------------------------------------------
+			AddImage(97, 49, 9005);
+			AddImageTiled(58, 39, 29, 390, 10460);
+			AddImageTiled(412, 37, 31, 389, 10460);
+			AddLabel(140, 60, 0x34, "Shirt Exchange");
+			//----------------------/----------------------------------------------/
+			AddHtml(107, 140, 300, 230, " < BODY > " +
+			"<BASEFONT COLOR=YELLOW>Greetings!<BR>" +
+			"<BASEFONT COLOR=YELLOW><BR>" +
+			"<BASEFONT COLOR=YELLOW>Your loyalty to the Queen is sufficient<BR>" +
+			"<BASEFONT COLOR=YELLOW>You can give me a Stevie shirt <BR>" +
+			"<BASEFONT COLOR=YELLOW>and I will exchange it for a Royal Gargish Tamers Sash!!!<BR>" +
+			"</BODY>", false, true);
+			//----------------------/----------------------------------------------/
+			AddImage(430, 9, 10441);
+			AddImageTiled(40, 38, 17, 391, 9263);
+			AddImage(6, 25, 10421);
+			AddImage(34, 12, 10420);
+			AddImageTiled(94, 25, 342, 15, 10304);
+			AddImageTiled(40, 427, 415, 16, 10304);
+			AddImage(-10, 314, 10402);
+			AddImage(56, 150, 10411);
+			AddImage(155, 120, 2103);
+			AddImage(136, 84, 96);
+			AddButton(225, 390, 0xF7, 0xF8, 0, GumpButtonType.Reply, 0);
+		}
+		//----------------------/----------------------------------------------/
+		public override void OnResponse(NetState state, RelayInfo info)
+		{
+			Mobile from = state.Mobile; switch (info.ButtonID)
+			{
+				case 0: { break; }
+			}
+		}
+	}
+
+	public class StevieShirtExchangerQuestGump2 : Gump
+	{
+		public static void Initialize()
+		{
+			CommandSystem.Register("StevieShirtExchangerQuestGump", AccessLevel.GameMaster, new CommandEventHandler(StevieShirtExchangerQuestGump_OnCommand2));
+		}
+		private static void StevieShirtExchangerQuestGump_OnCommand2(CommandEventArgs e)
+		{
+			e.Mobile.SendGump(new StevieShirtExchangerQuestGump2(e.Mobile));
+		}
+		public StevieShirtExchangerQuestGump2(Mobile owner) : base(50, 50)
+		{
+			//----------------------------------------------------------------------------------------------------
+			AddPage(0); AddImageTiled(54, 33, 369, 400, 2624); AddAlphaRegion(54, 33, 369, 400); AddImageTiled(416, 39, 44, 389, 203);
+			//--------------------------------------Window size bar--------------------------------------------
+			AddImage(97, 49, 9005);
+			AddImageTiled(58, 39, 29, 390, 10460);
+			AddImageTiled(412, 37, 31, 389, 10460);
+			AddLabel(140, 60, 0x34, "Shirt Exchange");
+			//----------------------/----------------------------------------------/
+			AddHtml(107, 140, 300, 230, " < BODY > " +
+			"<BASEFONT COLOR=YELLOW>Greetings!<BR>" +
+			"<BASEFONT COLOR=YELLOW><BR>" +
+			"<BASEFONT COLOR=YELLOW>Your loyalty to the Queen is not sufficient<BR>" +
+			"<BASEFONT COLOR=YELLOW>Return to me after you have served the Queen.<BR>" +
+			"</BODY>", false, true);
+			//----------------------/----------------------------------------------/
+			AddImage(430, 9, 10441);
+			AddImageTiled(40, 38, 17, 391, 9263);
+			AddImage(6, 25, 10421);
+			AddImage(34, 12, 10420);
+			AddImageTiled(94, 25, 342, 15, 10304);
+			AddImageTiled(40, 427, 415, 16, 10304);
+			AddImage(-10, 314, 10402);
+			AddImage(56, 150, 10411);
+			AddImage(155, 120, 2103);
+			AddImage(136, 84, 96);
+			AddButton(225, 390, 0xF7, 0xF8, 0, GumpButtonType.Reply, 0);
+		}
+		//----------------------/----------------------------------------------/
+		public override void OnResponse(NetState state, RelayInfo info)
+		{
+			Mobile from = state.Mobile; switch (info.ButtonID)
+			{
+				case 0: { break; }
+			}
+		}
+	}
+
+	public class StevieShirtExchangerQuestGump3 : Gump
+	{
+		public static void Initialize()
+		{
+			CommandSystem.Register("StevieShirtExchangerQuestGump", AccessLevel.GameMaster, new CommandEventHandler(StevieShirtExchangerQuestGump_OnCommand3));
+		}
+		private static void StevieShirtExchangerQuestGump_OnCommand3(CommandEventArgs e)
+		{
+			e.Mobile.SendGump(new StevieShirtExchangerQuestGump3(e.Mobile));
+		}
+		public StevieShirtExchangerQuestGump3(Mobile owner) : base(50, 50)
+		{
+			//----------------------------------------------------------------------------------------------------
+			AddPage(0); AddImageTiled(54, 33, 369, 400, 2624); AddAlphaRegion(54, 33, 369, 400); AddImageTiled(416, 39, 44, 389, 203);
+			//--------------------------------------Window size bar--------------------------------------------
+			AddImage(97, 49, 9005);
+			AddImageTiled(58, 39, 29, 390, 10460);
+			AddImageTiled(412, 37, 31, 389, 10460);
+			AddLabel(140, 60, 0x34, "Shirt Exchange");
+			//----------------------/----------------------------------------------/
+			AddHtml(107, 140, 300, 230, " < BODY > " +
+			"<BASEFONT COLOR=YELLOW>Foreign scum!<BR>" +
+			"<BASEFONT COLOR=YELLOW><BR>" +
+			"<BASEFONT COLOR=YELLOW>Be gone before I kill you<BR>" +
+			"</BODY>", false, true);
+			//----------------------/----------------------------------------------/
+			AddImage(430, 9, 10441);
+			AddImageTiled(40, 38, 17, 391, 9263);
+			AddImage(6, 25, 10421);
+			AddImage(34, 12, 10420);
+			AddImageTiled(94, 25, 342, 15, 10304);
+			AddImageTiled(40, 427, 415, 16, 10304);
+			AddImage(-10, 314, 10402);
+			AddImage(56, 150, 10411);
+			AddImage(155, 120, 2103);
+			AddImage(136, 84, 96);
+			AddButton(225, 390, 0xF7, 0xF8, 0, GumpButtonType.Reply, 0);
+		}
+		//----------------------/----------------------------------------------/
+		public override void OnResponse(NetState state, RelayInfo info)
+		{
+			Mobile from = state.Mobile; switch (info.ButtonID)
+			{
+				case 0: { break; }
+			}
+		}
+	}
+
+}
