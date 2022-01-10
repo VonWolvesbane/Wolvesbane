a74171b6511d17c8ca3829a288d300f09565a72f
Felix <credzba@gmail.com>
2022-01-09 21:05:32 -0600

    Add null check to avoid server crash

--- Scripts/Services/Craft/Core/CraftItem.cs
+++ Scripts/Services/Craft/Core/CraftItem.cs
@@ -2137,9 +2137,12 @@ namespace Server.Engines.Craft
 					if (OWLTROptionsManager.IsEnabled(OWLTROptionsManager.OPTIONS_ENUM.DEDICATION_RECIPE) && !UseAllRes)
 					{
 						NewDaat99Holder dh = (NewDaat99Holder)daat99.Daat99OWLTR.TempHolders[from];
+						if (dh != null)
+						{
 							++dh.ItemsCrafted;
-						if ( dh.ItemsCrafted >= 100 && dh.NextReward <= TimeSpan.Zero ) //give random recipe
-							dh.GiveDedication(from, craftSystem);
+							if (dh.ItemsCrafted >= 100 && dh.NextReward <= TimeSpan.Zero) //give random recipe
+								dh.GiveDedication(from, craftSystem);
+						}
 					}
 					//daat99 OWLTR end - crafting system
 
