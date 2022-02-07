cf63126768218309c78262c9c05b5919074a0dfa
VonWolvesbane <57732336+VonWolvesbane@users.noreply.github.com>
2022-02-06 19:41:19 -0500

    Create ValentinesCookies.cs

---
+++ Scripts/Custom/Wolvesbane Written/Holiday Items/Valentines/ValentinesCookies.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Server.Items
+{
+	[FlipableAttribute(0xC001, 0xC002)]
+	public class ValentineCookies : Cookies
+    {
+        [Constructable]
+        public ValentineCookies()
+			:base(0xC001)
+        {
+			
+			
+        }
+
+        public ValentineCookies(Serial serial)
+            : base(serial)
+        {
+        }
+
+        
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write((int)0); // version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+			int version = reader.ReadInt();
+		}
+    }
+}
