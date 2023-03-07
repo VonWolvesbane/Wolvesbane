2018b45bc6f5182d30f3573d2fcb630dd4b0eb1e
camo6660 <kurt.bauckham@hotmail.co.uk>
2023-03-07 19:39:05 +0000

    Update EvoMageAI.cs

--- Scripts/Custom/EVO System/AI/EvoMageAI.cs
+++ Scripts/Custom/EVO System/AI/EvoMageAI.cs
@@ -37,61 +37,85 @@ namespace Xanthos.Evo
 		}
 
 		public override bool DoOrderGuard()
-		{
-			if ( m_Mobile.IsDeadPet )
-				return true;
-
-			Mobile controlMaster = m_Mobile.ControlMaster;
-
-			if ( controlMaster == null || controlMaster.Deleted )
-				return true;
-
-			Mobile combatant = m_Mobile.Combatant as Mobile;
-
-			List<AggressorInfo> aggressors = controlMaster.Aggressors;
-
-			if ( aggressors.Count > 0 )
+					if (m_Mobile.IsDeadPet)
 			{
-				for ( int i = 0; i < aggressors.Count; ++i )
-				{
-					AggressorInfo info = aggressors[ i ];
-					Mobile attacker = info.Attacker;
-
-					if ( attacker != null && !attacker.Deleted && attacker.GetDistanceToSqrt( m_Mobile ) <= m_Mobile.RangePerception )
-					{
-						if ( combatant == null || attacker.GetDistanceToSqrt( controlMaster ) < combatant.GetDistanceToSqrt( controlMaster ) )
-						{
-							if ( (attacker is PlayerMobile && m_CanAttackPlayers) || !(attacker is PlayerMobile) )
-								combatant = attacker;
-						}
-					}
-				}
-
-				if ( combatant != null )
-					m_Mobile.DebugSay( "Crap, my master has been attacked! I will atack one of those bastards!" );
+				return true;
 			}
 
-			if ( combatant != null && combatant != m_Mobile && combatant != m_Mobile.ControlMaster && !combatant.Deleted && combatant.Alive && !combatant.IsDeadBondedPet && m_Mobile.CanSee( combatant ) && m_Mobile.CanBeHarmful( combatant, false ) && combatant.Map == m_Mobile.Map )
+			var controlMaster = m_Mobile.ControlMaster;
+
+			if (controlMaster == null || controlMaster.Deleted)
 			{
-				m_Mobile.DebugSay( "Guarding from target..." );
+				return true;
+			}
+
+			var combatant = m_Mobile.Combatant as Mobile;
+
+            if (combatant != null && !ValidGuardTarget(combatant))
+                combatant = null;
+
+            Mobile closestMob = combatant;
+            var closestDist = combatant == null ? m_Mobile.RangePerception : combatant.GetDistanceToSqrt(controlMaster);
+
+            foreach (var aggressor in controlMaster.Aggressors.Select(x => x.Attacker).Where(m => ValidGuardTarget(m)))
+            {
+                var dist = aggressor.GetDistanceToSqrt(controlMaster);
+
+                if (closestMob == null || dist < closestDist)
+                {
+                    closestMob = aggressor;
+                    closestDist = dist;
+                }
+            }
+
+            foreach (var aggressed in controlMaster.Aggressed.Select(x => x.Defender).Where(m => ValidGuardTarget(m)))
+            {
+                var dist = aggressed.GetDistanceToSqrt(controlMaster);
+
+                if (closestMob == null || dist < closestDist)
+                {
+                    closestMob = aggressed;
+                    closestDist = dist;
+                }
+            }
+
+            if (closestMob != null)
+            {
+                if (m_Mobile.Debug && closestMob != null && combatant != closestMob)
+                {
+                    m_Mobile.DebugSay("Crap, my master has been attacked! I will attack one of those bastards!");
+                }
+
+                combatant = closestMob;
+            }        
+
+            if (combatant != null)
+			{
+				m_Mobile.DebugSay("Guarding from target...");
 
 				m_Mobile.Combatant = combatant;
 				m_Mobile.FocusMob = combatant;
 				Action = ActionType.Combat;
+                m_Mobile.Direction = m_Mobile.GetDirectionTo(combatant);
 
-				/*
-				 * We need to call Think() here or spell casting monsters will not use
-				 * spells when guarding because their target is never processed.
-				 */
-				Think();
+                /*
+                * We need to call Think() here or spell casting monsters will not use
+                * spells when guarding because their target is never processed.
+                */
+                Think();
 			}
 			else
 			{
-				m_Mobile.DebugSay( "Nothing to guard from" );
+				m_Mobile.DebugSay("Nothing to guard from");
 
 				m_Mobile.Warmode = false;
 
-				WalkMobileRange( controlMaster, 1, false, 0, 1 );
+				if (Core.AOS)
+				{
+					m_Mobile.CurrentSpeed = m_Mobile.ActiveSpeed;
+                }
+
+				WalkMobileRange(controlMaster, 1, false, 0, 1);
 			}
 
 			return true;
