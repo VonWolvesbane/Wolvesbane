f8d611cde514d28b514c0ae5397dfd8aa585d6f5
VonWolvesbane <57732336+VonWolvesbane@users.noreply.github.com>
2023-04-22 21:58:26 -0400

    Fix Shrink

--- Scripts/Misc/ShrinkTable.cs
+++ Scripts/Misc/ShrinkTable.cs
@@ -48,7 +48,7 @@ namespace Server
                 return;
             }
 
-            m_Table = new int[1600];
+            m_Table = new int[1800];
 
             using (StreamReader ip = new StreamReader(path))
             {
