5568f0534eff721e5f50ecd63abfef0d670a819a
VonWolvesbane <57732336+VonWolvesbane@users.noreply.github.com>
2024-04-21 15:10:49 -0400

    Update ResourceInfo.cs

--- Scripts/Misc/ResourceInfo.cs
+++ Scripts/Misc/ResourceInfo.cs
@@ -1187,7 +1187,7 @@ namespace Server.Items
             //daat99 OWLTR end - custom leather
 
             //daat99 OWLTR start - custom ores
-            if (info.Level >= 0 && info.Level <= 13)
+            if (info.Level >= 0 && info.Level <= 14)
                 return (CraftResource)(info.Level + 1);
             //daat99 OWLTR end - custom ores
 
